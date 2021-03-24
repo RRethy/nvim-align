2766e8fe15697fb469f953786fcc39d1f55d06fc
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-03-24 11:29:29 -0400

    Adjust behaviour.
    I used to pad with whitespace for this scenario:
    
    hello
            hello
        hello
    
    It would move them left since the whitespace is just fluf, but I have
    now reverted.

--- lua/align.lua
+++ lua/align.lua
@@ -6,9 +6,7 @@ function M.align(pat)
 
     local max = -1
     local lines = vim.api.nvim_buf_get_lines(0, top[2]-1, bot[2], false)
-    for i, line in pairs(lines) do
-        line = string.gsub(line, '%s*'..pat, ' '..pat, 1)
-        lines[i] = line
+    for _, line in pairs(lines) do
         local s = re:match_str(line)
         if s and max < s then
             max = s
@@ -20,9 +18,9 @@ function M.align(pat)
     for i, line in pairs(lines) do
         local s = re:match_str(line)
         if s then
-            local rep = max - s + 1
+            local rep = max - s
             local newline = {
-                string.sub(line, 1, s-1),
+                string.sub(line, 1, s),
                 string.rep(' ', rep),
                 string.sub(line, s+1),
             }
