44d6480465f6aa95c8c4c9b74ac8f0690e009a03
avionix <avionix@gmx.com>
2021-10-15 02:36:58 -0400

    Add command for align_lines; change line arrangement
    Adjust arg count for AlignLines command definition
    
    Set -nargs to + for AlignLines
    
    Convert strings (passed from Vim command) to int

--- lua/align.lua
+++ lua/align.lua
@@ -8,8 +8,12 @@ function M.align(pat)
 end
 
 function M.align_lines(pat, startline, endline)
-    local re = vim.regex(pat)
+ 	local startline = tonumber(startline)
+	local endline   = tonumber(endline)
+
+	local re = vim.regex(pat)
     local max = -1
+
     local lines = vim.api.nvim_buf_get_lines(0, startline, endline, false)
     for _, line in pairs(lines) do
         local s = re:match_str(line)
--- plugin/align.vim
+++ plugin/align.vim
@@ -1,7 +1,6 @@
-if exists('g:loaded_align_nvim')
-  finish
+if !exists('g:loaded_align_nvim')
+	command! -range=% -nargs=1 Align lua require'align'.align(<f-args>)
+	command! -nargs=+ AlignLines lua require'align'.align_lines(<f-args>)
+	let g:loaded_align_nvim = 1
 endif
 
-command! -range=% -nargs=1 Align lua require'align'.align(<f-args>)
-
-let g:loaded_align_nvim = 1
